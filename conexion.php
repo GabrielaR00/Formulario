26d5cdb8aeb727de550f421a90197d5709ec51e7
Srocha8 <est.sabas.rocha@unimilitar.edu.co>
2021-03-11 09:45:25 -0500

    PHP CHIMBA

--- conexion.php
+++ conexion.php
@@ -1,19 +1,23 @@
 <?php
 
 $conex = mysqli_connect("localhost","root","","formulario");
+if (!$conex) {
+  echo "Fallo la conexion. ";
+}
+else {
+  echo "Se conectó correctamente. ";
+}
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$contraseña = $_POST['contraseña'];
 
-  $nombre = $_POST['nombre'];
-  $correo = $_POST['correo'];
-  $contraseña = $_POST['contraseña'];
-
-  $consulta = "INSERT INTO formulario(nombre, correo, contraseña) VALUES ('$nombre', '$correo', '$contraseña')";
-  $resultado = mysqli_query($conex, $consulta);
+$consulta = "INSERT INTO datosformulario(nombre, correo, contraseña) VALUES ('$nombre', '$correo', '$contraseña')";
+$resultado = mysqli_query($conex, $consulta);
 
 if (!$resultado) {
-    echo "se shingo algo";
-  }
-  else {
-    echo "melaaaaaaardo";
-  }
-
+  echo "Registro Fallido";
+}
+else {
+  echo "Registro Exitoso. ";
+}
  ?>
