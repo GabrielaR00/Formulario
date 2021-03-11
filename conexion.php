949563d6df8e69e7daebfc3e87e09e0c0d803f9b
Srocha8 <est.sabas.rocha@unimilitar.edu.co>
2021-03-11 08:56:40 -0500

    php fail2

--- conexion.php
+++ conexion.php
@@ -1,18 +1,19 @@
 <?php
+
+$conex = mysqli_connect("localhost","root","","formulario");
+
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
   $contraseña = $_POST['contraseña'];
 
-  $conexion = new mysqli('localhost','root','','formualrio');
-  if ($conexion->connect_error) {
-    die('Conection Failed :'.$conexion->connect_error);
+  $consulta = "INSERT INTO formulario(nombre, correo, contraseña) VALUES ('$nombre', '$correo', '$contraseña')";
+  $resultado = mysqli_query($conex, $consulta);
+
+if (!$resultado) {
+    echo "se shingo algo";
   }
   else {
-    $insertar = $conexion->prepare("intert into registration(nombre, correo, contraseña)VALUES(?,?,?)")
-    $insertar->bind_param("sssssi", $nombre, $correo, $contraseña);
-    $insertar->excute();
-    echo "Registro completado";
-    $insertar->close();
-    $conexion->close();
+    echo "melaaaaaaardo";
   }
+
  ?>
